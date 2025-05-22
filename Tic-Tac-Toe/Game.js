61c573db0a2fae5752fb6da0b559adfdf84bd7de
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-05-22 04:56:35 +0200

    Implemented the Game class and integrated it into main.js. The Tic Tac Toe game logic is now playable.

--- Tic-Tac-Toe/Game.js
+++ Tic-Tac-Toe/Game.js
@@ -10,6 +10,7 @@ export class Game {
     }
     this.player1 = player1;
     this.player2 = player2;
+    this.turn = 0;
 
     this.board = [
       [" ", " ", " "],
@@ -24,12 +25,7 @@ export class Game {
 
     const randomNumber = Math.floor(Math.random() * 2) + 1;
 
-    if (randomNumber === 1) {
-        return 1;
-    } else {
-        return 2;
-    }
-    
+    return randomNumber;
   }
 
   print() {
@@ -42,30 +38,111 @@ export class Game {
   }
 
   makeMove(player) {
-
     if (player !== this.player1 && player !== this.player2) {
-        throw new Error("Invalid Player! The players are: " + this.player1.name + " and " + this.player2.name);
+      throw new Error(
+        "Invalid Player! The players are: " +
+          this.player1.name +
+          " and " +
+          this.player2.name
+      );
     }
 
+    while (this.turn < 9) {
+      let win;
+      while (true) {
+        const { row, column } = player.makeTurn();
+        console.log(); //empty line
+        if (isNaN(row) || row < 1 || row > 3) {
+          console.log("Invalid row!");
+          continue;
+        }
+        if (isNaN(column) || column < 1 || column > 3) {
+          console.log("Invalid column!");
+          continue;
+        }
+        if (this.board[row - 1][column - 1] !== " ") {
+          console.log("The given place is not empty");
+          continue;
+        }
 
-    while (true) {
-      const { row, column } = player.makeTurn(input);
-
-      if (isNaN(row) || row < 1 || row > 3) {
-        console.log("Invalid row!");
-        continue;
-      }
-      if (isNaN(column) || column < 1 || column > 3) {
-        console.log("Invalid column!");
-        continue;
-      }
-      if (this.board[row - 1][column - 1] !== " ") {
-        console.log("The given place is not empty");
-        continue;
+        this.board[row - 1][column - 1] = player.symbol;
+        if (this.checkWin(player)) {
+          console.log(player, "wins!");
+          this.print();
+          return true; // Signal fürs Ende
+        }
+        break;
       }
 
-      this.board[row - 1][column - 1] = player.symbol;
-      break;
+      this.print();
+
+      this.turn++;
+
+      console.log();
+
+      //? Swap players
+      const nextPlayer = player === this.player1 ? this.player2 : this.player1;
+      return this.makeMove(nextPlayer);
     }
+  }
+
+  checkWin(player) {
+    const lines = [
+      [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+      ], //row 0
+      [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ], //row 1
+      [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+      ], //row 2
+
+      [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+      ], //column 0
+      [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ], //column 1
+      [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+      ], //column 2
+
+      [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+      ], //diagonal 0
+      [
+        [0, 2],
+        [1, 1],
+        [2, 0],
+      ], //diagonal 1
+    ];
+
+    for (let line of lines) {
+      let win = true;
+      for (let field of line) {
+        const [row, column] = field;
+        if (this.board[row][column] !== player.symbol) {
+          win = false;
+          break;
+        }
+      }
+      if (win) return true;
+    }
+    return false;
   }
 }
--- Tic-Tac-Toe/Main.js
+++ Tic-Tac-Toe/Main.js
@@ -4,29 +4,43 @@ import promptSync from "prompt-sync";
 
 const input = promptSync();
 
+let p1;
+let p2;
+while (true) {
+  try {
+
 const p1Name = input("Player 1, what's your name? ");
 const symbol = input("What symbol do you want to play with? ");
 const p2Name = input("Player 2, what's your name? ");
 
-let p1;
-let p2;
 
-if (typeof symbol === "string") {
-  if (symbol.toLowerCase() === "x") {
-    p1 = new Player(p1Name, symbol);
-    p2 = new Player(p2Name, "O");
-  } else if (symbol.toLowerCase() === "o") {
-    p1 = new Player(p1Name, symbol);
-    p2 = new Player(p2Name, "X");
-  } else {
-    throw new Error("Invalid symbol: must be X or O!");
+
+    if (typeof symbol === "string") {
+      if (symbol.toLowerCase() === "x") {
+        p1 = new Player(p1Name, symbol);
+        p2 = new Player(p2Name, "O");
+      } else if (symbol.toLowerCase() === "o") {
+        p1 = new Player(p1Name, symbol);
+        p2 = new Player(p2Name, "X");
+      } else {
+        throw new Error("Invalid symbol: must be X or O!");
+      }
+    } else {
+      throw new Error("Invalid symbol: must be string");
+    }
+    break;
+  } catch (error) {
+    console.log("An error occured.", error.message);
+    continue;
   }
-} else {
-  throw new Error("Invalid symbol: must be string");
 }
 
 const game = new Game(p1, p2);
 const start = game.start();
+
+console.log();
+game.print();
+console.log();
 
 if (start === 1) {
   console.log("Player 1 will start!");
@@ -35,7 +49,3 @@ if (start === 1) {
   console.log("Player 2 will start!");
   game.makeMove(p2);
 }
-
-game.print();
-
-
